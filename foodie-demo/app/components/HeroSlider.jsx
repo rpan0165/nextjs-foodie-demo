a6bd43b2367912c57826888de600ceb5c01c9f5e
rpan0165 <105200115+rpan0165@users.noreply.github.com>
2023-04-12 17:38:35 +0530

    Update HeroSlider.jsx

--- foodie-demo/app/components/HeroSlider.jsx
+++ foodie-demo/app/components/HeroSlider.jsx
@@ -51,7 +51,7 @@ function HeroSlider(heroSliderContent) {
               <SwiperSlide key={index}>
                 <div className="swiper-slide-container bg-white">
                   <div className="text-container">
-                    <h2 className='text-3xl font-extrabold font-serif'>{sliceStrings(slide.header)[0]}<br />{sliceStrings(slide.header)[1]}</h2>
+                    <h2 className='text-4xl font-extrabold font-serif'>{sliceStrings(slide.header)[0]}<br />{sliceStrings(slide.header)[1]}</h2>
                     <button className='learn-btn'>{slide.cta.title}</button>
                   </div>
                   <div className="image-container">
