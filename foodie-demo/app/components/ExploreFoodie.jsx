7311438de01bead2970bbfadce56f073ebc9c207
rpan0165 <105200115+rpan0165@users.noreply.github.com>
2023-04-18 14:15:21 +0530

    Adding explore section

---
+++ foodie-demo/app/components/ExploreFoodie.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import Book from '../assets/images/explore-foodie-logos/book.png';
+import Brands from '../assets/images/explore-foodie-logos/brands.png';
+import Diamond from '../assets/images/explore-foodie-logos/diamond.png';
+import Heart from '../assets/images/explore-foodie-logos/heart.png';
+import Idea from '../assets/images/explore-foodie-logos/idea.png';
+import People from '../assets/images/explore-foodie-logos/people.png';
+import Restaurant from '../assets/images/explore-foodie-logos/restaurant.png';
+import Star from '../assets/images/explore-foodie-logos/star.png';
+import Testimonials from '../assets/images/explore-foodie-logos/testimonials.png';
+import Tips from '../assets/images/explore-foodie-logos/tips.png';
+import Image from 'next/image';
+
+function ExploreFoodie() {
+    return (
+        <div>
+            <h2 className="heading-circles text-4xl font-bold" style={{ fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif" }}>Great Ways To Explore Sysco Foodie</h2>
+            <br />
+            <br />
+            <div className='grid grid-flow-row-dense lg:grid-cols-5 md:grid-cols-5 sm:grid-cols-2 mx-auto px-5 text-center font-bold'>
+                <div className='my-4'>
+                    <Image src={Tips} width={200} height={200} className='mx-auto' />
+                    <h5 className='my-3'>Tips & Trends</h5>
+                </div>
+                <div className='my-4'>
+                    <Image src={Brands} width={200} height={200} className='mx-auto' />
+                    <h5 className='my-3'>Sysco Brands</h5>
+                </div>
+                <div className='my-4'>
+                    <Image src={Idea} width={200} height={200} className='mx-auto' />
+                    <h5 className='my-3'>Cutting Edge Solutions</h5>
+                </div>
+                <div className='my-4'>
+                    <Image src={People} width={200} height={200} className='mx-auto my-3 py-1' />
+                    <h5 className='my-3'>Sysco Exclusive Webinars</h5>
+                </div>
+                <div className='my-4'>
+                    <Image src={Testimonials} width={200} height={200} className='mx-auto my-3 py-1' />
+                    <h5 className='my-3'>Testimonials</h5>
+                </div>
+                <div className='my-4'>
+                    <Image src={Book} width={200} height={200} className='mx-auto' />
+                    <h5 className='my-3'>Recipes</h5>
+                </div>
+                <div className='my-4'>
+                    <Image src={Diamond} width={200} height={200} className='mx-auto' />
+                    <h5 className='my-3'>Foodie Solutions</h5>
+                </div>
+                <div className='my-4'>
+                    <Image src={Restaurant} width={200} height={200} className='mx-auto' />
+                    <h5 className='my-3'>Virtual Restaurant</h5>
+                </div>
+                <div className='my-4'>
+                    <Image src={Heart} width={200} height={200} className='mx-auto my-3 py-1' />
+                    <h5 className='my-3'>Send Us A Story</h5>
+                </div>
+                <div className='my-4'>
+                    <Image src={Star} width={200} height={200} className='mx-auto my-3 py-1' />
+                    <h5 className='my-3'>Become a Customer</h5>
+                </div>
+                
+            </div>
+            <br /><br />
+            <hr />
+            <br />
+        </div>
+    )
+}
+
+export default ExploreFoodie
--- foodie-demo/app/components/MagazineSection.jsx
+++ foodie-demo/app/components/MagazineSection.jsx
@@ -5,22 +5,29 @@ const MagazineSection = (magazineContent) => {
   const magazines = magazineContent.props.magazine_section.category;
 
   return (
-    <div className='grid grid-cols-2' style={{ backgroundColor: "#2A2F33" }}>
-      <div className='col-span-1'>
-        <h1 className='text-white'>Column 1</h1>
-        <h3 className="text-left text-white font-bold text-md" style={{ fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif" }}>The Latest Edition of Foodie</h3>
-                <div class="h_line mt-0 mb-4 pt-2 " style={{
-                    color: "red", borderBottom: "5px solid #56B146",
-                    width: "10rem",
-                }}></div>
-           <h2 className="text-left text-white font-bold text-md" style={{ fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif" }}>The Latest Edition of Foodie</h2>
-                <div class="h_line mt-0 mb-4 pt-2 " style={{
-                    color: "red", borderBottom: "5px solid #56B146",
-                    width: "10rem", margin: "auto",
-                }}></div>
-      </div>
-      <div className='col-span-1'>
-        <h1 className='text-white'>Column 2</h1>
+    <div style={{ backgroundColor: "#2A2F33" }}>
+      <div className='grid grid-cols-2 mx-auto' style={{ backgroundColor: "#2A2F33", width: "70%" }}>
+        <div className='col-span-1 ml-5'>
+          <br />
+          <h3 className="text-left text-white font-bold text-lg" style={{ fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif" }}>The Latest Edition of Foodie</h3>
+          <div class="h_line mt-0 mb-4 pt-2 " style={{
+            borderBottom: "3px solid #56B146",
+            width: "10rem",
+          }}></div>
+          <h4 className="text-left text-white font-bold text-md">ON THE COVER</h4>
+          <h2 className="text-left text-white font-bold text-4xl" style={{ fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif" }}>Foodie Magazine</h2>
+          <h4 className="text-left text-white text-xl mt-1" style={{ fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif" }}>People | Products | Platforms</h4>
+          <div class="h_line_thin mt-0 mb-4 " style={{
+            borderBottom: "3px solid #56B146",
+            width: "10rem", height: "2rem"
+          }}></div>
+          <div>
+            
+          </div>
+        </div>
+        <div className='col-span-1'>
+          <h1 className='text-white'>Column 2</h1>
+        </div>
       </div>
     </div>
   )
