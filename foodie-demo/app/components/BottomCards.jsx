8b6ed98b44f34ca40a14d6080c7d8ca1fc8e21b6
Charu Palihawatta <charu.palihawatta@syscolabs.com>
2023-04-23 23:36:10 +0530

    adding updated BottomCards component

--- foodie-demo/app/components/BottomCards.jsx
+++ foodie-demo/app/components/BottomCards.jsx
@@ -1,15 +1,49 @@
+'use client'
 import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+
 
 function BottomCards() {
-    return (
+    const [cards, setCards] = useState([]);
+    const [loading, setLoading] = useState(true);
+  
+    useEffect(() => {
+  
+      const fetchCards = async () => {
+        setLoading(true);
+        const res = await axios.get('https://cdn.contentstack.io/v3/content_types/foodie_bottomcards/entries?environment=development',
+          {
+            headers: {
+              'api_key': 'blt16b29db83ad01635',
+              'access_token': 'cse3066d807437d70a5cc6bee6',
+              'Content-Type': 'application/json',
+            }
+          }
+        );
+  
+        setCards(res.data.entries[0].card);
+        setLoading(false);
+      };
+  
+      fetchCards();
+      console.log("fetched")
+  
+    }, []);
+  
+    return loading && cards.length == 0 ? (
+      <div className="spinner-UI">
+        <p>loading</p>
+      </div>
+    ) : (
         <div className="grid grid-cols-12 grid-flow-row sm:grid-flow-col justify-center mt-5" style={{width:"85%", margin:"0 auto", marginBottom:"5rem"}}>
             <div className=' col-span-6 flex flex-row border-2 border-orange-200 rounded-xl' style={{ margin: "12px", backgroundColor: "#FFF9F0" }}>
                 
-                <div style={{ width: "50%", borderRadius: "10px 0 0 10px", height: "21rem", backgroundColor: "yellow", backgroundImage: "url('https://foodie.sysco.com/wp-content/uploads/2023/02/Plant-Based-Campaign-Banner-Foodie-3907536-Corporate.jpg')" }}>
+                <div style={{ width: "50%", borderRadius: "10px 0 0 10px", height: "21rem", backgroundColor: "yellow", backgroundImage: `url(${cards[0].image.url})`}}>
                 </div>
-                <div className='p-3 text-center overflow-auto' style={{width:"50%", height: "21rem", }}>
+                <div className='p-3 overflow-auto' style={{width:"50%", height: "21rem", }}>
 
-                    <h3 className='font-bold text-lg text-black' style={{ fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif", marginTop:"45%"}}>Opportunities with Plant-based Options</h3>
+                    <h3 href={cards[0].link.href} className='font-bold text-lg text-black' style={{ fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif", marginTop:"45%"}}>{cards[0].link.title}</h3>
 
                 </div>
 
@@ -18,11 +52,11 @@ function BottomCards() {
 
             <div className='col-span-6 flex flex-row border-2 border-orange-200 rounded-xl' style={{ margin: "12px", backgroundColor: "#FFF9F0" }}>
               
-                <div style={{ width: "50%", borderRadius: "10px 0 0 10px", height: "21rem", backgroundColor: "yellow", backgroundImage: "url('https://foodie.sysco.com/wp-content/uploads/2023/04/MicrosoftTeams-image-197.jpg')" }}>
+                <div style={{ width: "50%", borderRadius: "10px 0 0 10px", height: "21rem", backgroundColor: "yellow", backgroundImage: `url(${cards[1].image.url})` }}>
                 </div>
-                <div className='p-3 text-center overflow-auto' style={{width:"50%", height: "21rem" }}>
+                <div className='p-3 overflow-auto' style={{width:"50%", height: "21rem" }}>
 
-                    <h3 className='font-bold text-lg text-black' style={{ fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif", marginTop:"45%" }}>Mother's Day Menu Ideas</h3>
+                    <h3 href={cards[1].link.href} className='font-bold text-lg text-black' style={{ fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif", marginTop:"45%" }}>{cards[1].link.title}</h3>
 
                 </div>
 
--- foodie-demo/app/components/Community.jsx
+++ foodie-demo/app/components/Community.jsx
@@ -37,7 +37,7 @@ function Community(communityContent) {
                       <div style={{ width: "100%", height: "17.5rem", backgroundColor: "yellow", backgroundImage: `url(${card.image.url})` }}></div>
                       <div className='p-3' style={{ height: "8rem" }}>
                         <h4 className='text-sm text-sky-500'>{card.link.title}</h4>
-                        <h3 className='text-md pt-0 font-bold'>{card.link2.title}</h3>
+                        <h3 className='pt-0 font-bold'>{card.link2.title}</h3>
                       </div>                                         
                     </div>
                   ))
