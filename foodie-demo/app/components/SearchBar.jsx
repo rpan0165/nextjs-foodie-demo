ee5d41c7b556791d3fb75819a0ee04515b40a8b4
Charu Palihawatta <charu.palihawatta@syscolabs.com>
2023-04-23 23:44:28 +0530

    updated the width of search bar

--- foodie-demo/app/components/SearchBar.jsx
+++ foodie-demo/app/components/SearchBar.jsx
@@ -68,6 +68,7 @@ const SearchBar = () => {
             placeholder="Search for recipes, ingredients and more"
             width=""
             onChange={handleSearchInput}
+            style={{width: "820px" }}
           />
           {visible && searchQuery !== '' && (
             <div className="search-popup ml-5" style={{ width: "32%", marginLeft: "12rem" }}>
