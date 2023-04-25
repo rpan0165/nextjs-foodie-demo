cff8a3c56517d10b40f8caed0e2132bb5c8f782e
Charu Palihawatta <charu.palihawatta@syscolabs.com>
2023-04-25 13:53:29 +0530

    adding new updates component and style changes

--- foodie-demo/app/components/Updates.jsx
+++ foodie-demo/app/components/Updates.jsx
@@ -1,7 +1,5 @@
 'use client'
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 
 function Updates(cardContent) {
 
@@ -12,36 +10,23 @@ function Updates(cardContent) {
             <div class="container-fluid">
                 <h2 className="text-center font-bold text-4xl" style={{ fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif" }}>Foodservice Update</h2>
                 <p className="text-center pb-2" style={{ fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif", fontSize: "1.3rem" }}>Stay informed of current supply and demand changes in the market</p>
-                <div class="h_line mt-0 mb-4 pt-0 " style={{
-                    color: "red", borderBottom: "5px solid #56B146",
-                    width: "10rem",
-                    margin: "auto",
-                }}></div>
-                {/* <div className='green-line text-center pb-3 content-center text-green-500'><hr width="20%" ></hr></div> */}
-
-                <div class="row mx-auto pr-8 pl-8 pt-4 max-w-7xl">
-                    {
-                        cards.map(card => (
-                            <div class="col">
-                                <Card style={{ backgroundColor: "#FFF9F0"}}>
-                                    <Card.Img variant="top" src={card.image.url} />
-                                    <Card.Body>
-                                        <Card.Title className="text-sm text-center text-sky-500">{card.link[0].title}</Card.Title>
-                                        <Card.Text className="text-center pt-0 font-bold">
-                                            {card.link[1].title}
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </div>
-                        ))
-                    }
+                <br></br>
+                <div className='grid grid-cols-4 mr-5 mb-5 mx-auto' style={{ width: "85%" }}>
+                {
+                  cards.map(card => (
+                    <div className='flex flex-col border-2 rounded-lg' style={{ margin: "12px", backgroundColor: "#FFF9F0" }}>
+                      <div className="bg-cover" style={{ width: "100%", height: "17.5rem", backgroundColor: "yellow", backgroundImage: `url(${card.image.url})` }}></div>
+                      <div className='p-3' style={{ height: "6rem" }}>
+                        <h4 className='text-sm text-sky-500'>{card.link1.title}</h4>
+                        <h3 className='pt-0 font-bold'>{card.link2.title}</h3>
+                      </div>  
+                    </div>
+                  ))
+                }
                 </div>
-               
-
             </div>
             <br></br>
-            <br></br>
-        </div >
+        </div>
     );
 }
 
