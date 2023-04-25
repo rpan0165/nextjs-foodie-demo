89bf59be054fbfcb1476f4f7183cf9a6e6a004aa
Charu Palihawatta <charu.palihawatta@syscolabs.com>
2023-04-25 23:44:08 +0530

    adding updated cardgridsection

--- foodie-demo/app/components/CardGridSection.jsx
+++ foodie-demo/app/components/CardGridSection.jsx
@@ -2,31 +2,7 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import '../styles/cardgridsection.css';
-// import { GoSearch } from 'react-icons/go';
-// import { FaRegDotCircle } from 'react-icons/fa';
-// import { AiOutlineInstagram } from 'react-icons/io5';
-
-
-// const images = [
-//     { id: 1, url: 'https://via.placeholder.com/150' },
-//     { id: 2, url: 'https://via.placeholder.com/150' },
-//     { id: 3, url: 'https://via.placeholder.com/150' },
-//     { id: 4, url: 'https://via.placeholder.com/150' },
-//     { id: 5, url: 'https://via.placeholder.com/150' },
-//     { id: 6, url: 'https://via.placeholder.com/150' },
-//     { id: 7, url: 'https://via.placeholder.com/150' },
-//     { id: 8, url: 'https://via.placeholder.com/150' },
-//     { id: 9, url: 'https://via.placeholder.com/150' },
-//     { id: 10, url: 'https://via.placeholder.com/150' },
-//     { id: 11, url: 'https://via.placeholder.com/150' },
-//     { id: 12, url: 'https://via.placeholder.com/150' },
-//     { id: 13, url: 'https://via.placeholder.com/150' },
-//     { id: 14, url: 'https://via.placeholder.com/150' },
-//     { id: 15, url: 'https://via.placeholder.com/150' },
-//     { id: 16, url: 'https://via.placeholder.com/150' },
-//     { id: 17, url: 'https://via.placeholder.com/150' },
-//     { id: 18, url: 'https://via.placeholder.com/150' },
-// ];
+import { FaInstagram } from 'react-icons/fa';
 
 const Card = ({ id, url }) => {
     const [hovered, setHovered] = useState(false);
@@ -55,27 +31,27 @@ const CardGridSection = (cardGridContent) => {
         <>
             <h2 className="text-center font-bold text-4xl py-2 mt-3" style={{ fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif" }}>Connect With @SyscoFoodie</h2>
             <p className="text-center py-3" style={{ fontSize: "1.3rem" }}>Follow us on Instagram to see the relevant information regarding Sysco's latest resources for restaurant operators.</p>
-            {/* <div class="h_line mt-0 mb-4 pt-0 " style={{
-                color: "red", borderBottom: "5px solid #56B146",
-                width: "10rem",
-                margin: "auto",
-            }}></div> */}
-            <span>
-                <p className="text-center text-lg"> 
-                <span>
-                </span> LIVE
-                    Real-time updates and fresh content |<span>  </span> 
-                    #syscofoodie
-                    </p>
+            <span className="flex items-center justify-center text-center text-lg">
+                <span className="text-red-600 text-2xl mr-2">&#9673;</span>
+                <span className='font-bold text-sm'>LIVE</span>
+                <span className="ml-2">Real-time updates and fresh content | 
+                <a href="https://www.instagram.com/syscofoodie/" className="no-hover-effect"><FaInstagram className='inline-block mx-2' /></a>
+                <a href="https://www.instagram.com/syscofoodie/" className="no-hover-effect">#syscofoodie</a>
+                </span>
             </span>
-            <div className="card-grid mt-3" style={{ margin: "1rem 21rem"}}>
-
+            <br></br>
+            <div className='flex flex-col items-center'>
+                <div className="card-grid mt-3" >
                 {photos.slice(0, numRows * 3).map((photo) => (
                     <Card key={photo.id} id={photo.id} url={photo.images.url}  />
                 ))}
-                {/* {numRows * 3 < photos.length && (
-                    <button onClick={() => setNumRows(numRows + 4)} className='block mx-auto bg-green-400 px-4 py-2 font-bold rounded-sm hover:bg-green-500'>Load More</button>
-                )} */}
+                </div>
+                {numRows * 3 < photos.length && (
+                    <button 
+                        onClick={() => setNumRows(numRows + 4)} 
+                        className='block mx-auto bg-green-400 px-4 py-2 font-bold rounded-sm hover:bg-green-500'>Load More
+                    </button>
+                )}
             </div>
         </>
     );
