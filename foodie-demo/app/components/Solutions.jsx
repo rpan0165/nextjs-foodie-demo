9367e8841d8ffa329b41e0480f25cce587ad940a
nrat9963 <nrat9963@sysco.com>
2023-04-26 17:45:26 +0530

    cleanup

--- foodie-demo/app/components/Solutions.jsx
+++ foodie-demo/app/components/Solutions.jsx
@@ -15,13 +15,13 @@ function Solutions(recipeContent) {
               <div className="grid grid-cols-4 mr-5 mb-5 mx-auto">
                 {
                   recipes.map(recipe => (
-                    <div className='flex flex-col border-2 rounded-lg' style={{ margin: "12px", backgroundColor: "#FFF9F0" }}>
-                      <div className='p-3' style={{ height: "9rem" }}>
+                    <div className="flex flex-col border-2 rounded-lg m-3 bg-[#FFF9F0] overflow-hidden">
+                      <div className="p-3 h-36 flex flex-column justify-center">
                         <h3 href={recipe.link.href} className="text-sm font-bold text-sky-500">{recipe.link.title} </h3>
-                        <h4 className='text-black font-bold my-2 text-lg' style={{ fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif" }}>{recipe.header}</h4>
-                        <h3 className='text-sm text-black' style={{ fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif" }}>{recipe.subheader}</h3>
+                        <h4 className='text-black font-bold my-2 text-lg font-serif'>{recipe.header}</h4>
+                        <h3 className='text-sm text-black font-serif'>{recipe.subheader}</h3>
                       </div>
-                      <div className="bg-cover" style={{ width: "100%", borderRadius: "0 0 7px 7px", height: "17.5rem", backgroundColor: "yellow", backgroundImage: `url(${recipe.background_image.url})` }}>
+                      <div className="bg-cover w-full h-72 bg-[#FFF9F0]" style={{ backgroundImage: `url(${recipe.background_image.url})` }}>
                       </div>
                     </div>
                   ))
